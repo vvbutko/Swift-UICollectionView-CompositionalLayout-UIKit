8861ff950eba2e20c2e6afb82d548cab8a11deed
Vladimir Butko <vvbutko@gmail.com>
2022-04-03 16:58:49 -0400

    Initial Commit

---
+++ Swift-UICollectionView-CompositionalLayout-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-UICollectionView-CompositionalLayout-UIKit
+//
+//  Created by Vladimir Butko on 2022-04-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
